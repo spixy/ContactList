8dba63707085c5422ac6bddcf1069e8f09a3e8cb
Henrich Horváth <dev@henrich-horvath.eu>
2017-05-28 00:38:12 +0200

    fixes

--- app/src/main/java/contactlist/spixy/android/uib/contactlist/ContactFragment.java
+++ app/src/main/java/contactlist/spixy/android/uib/contactlist/ContactFragment.java
@@ -1,7 +1,5 @@
 package contactlist.spixy.android.uib.contactlist;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
